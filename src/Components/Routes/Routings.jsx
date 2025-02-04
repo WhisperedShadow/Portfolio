100641dd1f96746cadef4ee21818cc2d2e6ca8f3
WhisperedShadow <dd9514371@gmail.com>
2025-02-04 09:46:11 +0530

    The SEO is optimized and The sitemap generator is added

--- src/Components/Routes/Routings.jsx
+++ src/Components/Routes/Routings.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Home from '../pages/Home/Home.jsx'
 import About from '../pages/About/About.jsx'
 import Education from '../pages/Education/Education.jsx'
@@ -15,6 +15,7 @@ const Routings = () => {
       <Route path="/skills" element={<Skills/>}/>
       <Route path="/projects" element={<Projects/>}/>
       <Route path="/contact" element={<Contact/>}/>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
